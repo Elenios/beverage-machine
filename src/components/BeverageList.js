1747d89f24ec14d6c0dd5c71f69c8833ff3910fa
elenios <gabor.dobrozemszky@hotmail.com>
2018-10-30 22:49:27 +0100

    add key value to Beverage

--- src/components/BeverageList.js
+++ src/components/BeverageList.js
@@ -2,6 +2,6 @@ import React from 'react';
 import Beverage from './Beverage'
 import store from '../store';
 
-const BeverageList = ( {beverages = store} ) => beverages.map( beverage => <Beverage name={beverage.name} img={beverage.smallimg}/>);
+const BeverageList = ( {beverages = store} ) => beverages.map( beverage => <Beverage key={beverage.id} name={beverage.name} img={beverage.smallimg}/>);
 
 export default BeverageList;
