7b59077d71e004a564fe98894d76bdea2f8d554f
elenios <gabor.dobrozemszky@hotmail.com>
2018-11-04 12:19:18 +0100

    ongoing experiment, not working

--- src/index.js
+++ src/index.js
@@ -5,23 +5,25 @@ import AppContainer from './components/AppContainer';
 import { Provider } from 'react-redux';
 import store from './store';
 import { BrowserRouter, Route } from 'react-router-dom';
-// import Coke from './component/Coke';
-// import CokeZero from './component/CokeZero';
-// import Fanta from './component/Fanta';
-// import Sprite from './component/Sprite';
-// import Kinley from './component/Kinley';
-// import DrPepper from './component/DrPepper';
+import  Coke  from './components/Coke';
+// import CokeZero from './components/CokeZero';
+// import Fanta from './components/Fanta';
+// import Sprite from './components/Sprite';
+// import Kinley from './components/Kinley';
+// import DrPepper from './components/DrPepper';
 
 ReactDOM.render(
   <Provider store={ store }>
     <BrowserRouter>
+      <div>
         <Route exact path="/" component={ AppContainer } />
-        {/* <Route path="/coke" component={ Coke } />
-        <Route path="/cokezero" component={ CokeZero } />
+        <Route path="/coke" component={ Coke } />
+        {/* <Route path="/cokezero" component={ CokeZero } />
         <Route path="/fanta" component={ Fanta } />
         <Route path="/sprite" component={ Sprite } />
         <Route path="/kinley" component={ Kinley } />
         <Route path="/drpepper" component={ DrPepper } /> */}
+      </div>
     </BrowserRouter>
   </Provider>,
   document.getElementById( 'root' )
